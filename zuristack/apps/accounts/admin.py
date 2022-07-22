d869286c10d3a290911d8714affecfe45281b9ad
MAV3R1CK <eddymav247@gmail.com>
2022-07-22 18:11:17 +0100

    custom user model completed

--- zuristack/apps/accounts/admin.py
+++ zuristack/apps/accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+from . import forms
 
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = forms.CustomUserCreationForm
+    form = forms.CustomUserChangeForm
+    ordering = ['email']
+    list_display = ["email", "is_staff", "is_active", "is_superuser"]
+    search_fields = ["email"]
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('Details', {
+            "fields": (
+                'first_name', 'last_name', 'slug',
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff', 'is_active',
+            ),
+        }),
+    )
 # Register your models here.
